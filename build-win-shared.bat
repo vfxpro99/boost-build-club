d912d1ba1c354e8b6fb621604a637e1545a516da
Jeremy Cowles <jcowles@google.com>
2016-09-13 09:28:37 -0700

    Use multiple threads in boost batch files

--- build-win-shared.bat
+++ build-win-shared.bat
@@ -9,4 +9,4 @@ echo b2 --toolset=msvc-14.0 --build-type=complete stage link=shared
 echo
 echo Building boost DLLs for VS2010, x64
 echo
-b2 --toolset=msvc-14.0 --build-type=complete architecture=x86 address-model=64 stage link=shared
+b2 -j 16 --toolset=msvc-14.0 --build-type=complete architecture=x86 address-model=64 stage link=shared
