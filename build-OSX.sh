daa69df735c6211164f5e29b7ccbdc35e2be2fdd
vfxpro99 <vfxpro99@gmail.com>
2016-11-15 13:48:05 -0800

    build boost for x64 only

--- build-OSX.sh
+++ build-OSX.sh
@@ -2,15 +2,15 @@
 ./bootstrap.sh
 
 echo;echo;echo
-echo Building boost for OSX
+echo Building boost for 64 bit macOS
 echo;echo;echo
 
 cp boost-build-club-project-config.jam project-config.jam
 
-./b2 -j16 --build-dir=build-OSX     --stagedir=stage-OSX   --toolset=clang \
-     cxxflags="-std=c++11 -stdlib=libc++ -arch i386 -arch x86_64" linkflags="-stdlib=libc++" \
+./b2 -j16 --build-dir=build-OSX --stagedir=stage-OSX --toolset=clang \
+     cxxflags="-std=c++11 -stdlib=libc++ -arch x86_64" linkflags="-stdlib=libc++" \
      link=shared threading=multi stage
 
 echo;echo;echo
-echo Finished building boost for OSX as dylibs
+echo Finished building boost for 64 bit macOS as dylibs
 echo;echo;echo
