be6730fce052d6064a176b73d1b5da095f1ad5d7
vfxpro99 <vfxpro99@gmail.com>
2016-11-17 19:56:20 -0800

    Point at the boost in Maya

--- build-macos-maya-shared.sh
+++ build-macos-maya-shared.sh
@@ -1,5 +1,8 @@
 
-./bootstrap.sh
+./bootstrap.sh \
+    --with-python-version=2.7 \
+    --with-python-root=/Applications/Autodesk/maya2017/Maya.app/Contents/Frameworks/Python.framework/Versions/2.7 \
+    --with-python=/Applications/Autodesk/maya2017/Maya.app/Contents/Frameworks/Python.framework/Versions/2.7/bin/python2.7
 
 echo;echo;echo
 echo Building boost for OSX
@@ -9,6 +12,7 @@ cp boost-build-club-project-config.jam project-config.jam
 
 ./b2 -j16 --build-dir=build-OSX     --stagedir=stage-OSX   --toolset=clang \
      cxxflags="-std=c++11 -stdlib=libc++ -arch x86_64" linkflags="-stdlib=libc++" \
+     include=/Applications/Autodesk/maya2017/Maya.app/Contents/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
      link=shared threading=multi stage
 
 echo;echo;echo
