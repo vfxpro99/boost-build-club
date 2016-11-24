b4d002644d21728b6cfe455552d7c2a933cfdf5e
vfxpro99 <vfxpro99@gmail.com>
2016-11-24 11:15:17 -0800

    Cleaner boost python building

--- build-win-shared.bat
+++ build-win-shared.bat
@@ -1,12 +1,11 @@
 
 call bootstrap.bat
-copy boost-build-club-project-config.jam project-config.jam
+copy bbc-config-win-shared.jam project-config.jam
 
 echo
-echo Building boost DLLs for VS2010, x86
-echo
+echo To build boost DLLs for VS2015, x86:
 echo b2 --toolset=msvc-14.0 --build-type=complete stage link=shared
 echo
-echo Building boost DLLs for VS2010, x64
+echo Building boost DLLs for VS2015, x64
 echo
 b2 -j 16 --toolset=msvc-14.0 --build-type=complete architecture=x86 address-model=64 stage link=shared
